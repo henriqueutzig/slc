faea5a94212aa5d09cf5e78f2123af7b81ca4964
Henrique Utzig <henrique.utzig@sap.com>
2024-10-07 18:49:51 -0300

    Add draft test bash script

---
+++ test.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+INPUTS=$(ls tests/e1 | grep entrada_)
+mkdir output/
+
+for file in $INPUTS; do
+  cat tests/e1/$file >  output/output_$file.txt
+  cat tests/e1/$file | ./etapa1 >> output/output_$file.txt
+  # Your code to process each file
+done

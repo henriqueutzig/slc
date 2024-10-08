413f675653e2959e660a400d583c1caca2e497cb
Henrique Utzig <henrique.utzig@sap.com>
2024-10-08 15:29:43 -0300

    Fix reorg files TODO: fix makefile

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ mkdir output/
 
 for file in $INPUTS; do
   cat tests/e1/$file >  output/output_$file.txt
+  echo '\n' >> output/output_$file.txt
   cat tests/e1/$file | ./etapa1 >> output/output_$file.txt
   # Your code to process each file
 done

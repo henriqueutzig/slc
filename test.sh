fda306fd39c9cfd387f7582a5bf35a28eee58850
JoaoCosme <cosmejoaopedro@protonmail.com>
2024-10-13 11:06:53 -0300

    test script agora printa o erro no output

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/bash
 
 INPUTS=$(ls tests/e2 | grep entrada_)
-mkdir output/
+mkdir -p output/
 
 for file in $INPUTS; do
   cat tests/e2/$file >  output/output_$file.txt
   echo '\n' >> output/output_$file.txt
-  cat tests/e2/$file | ./etapa2 >> output/output_$file.txt
-  # Your code to process each file
+  if ! cat tests/e2/$file | ./etapa2 >> output/output_$file.txt 2>> output/output_$file.txt; then
+    echo "An error occurred while processing $file with etapa2." >> output/output_$file.txt
+  fi
 done

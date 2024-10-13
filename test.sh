fad32d2994445a61065b17c65b24fa8fad0836f2
JoaoCosme <cosmejoaopedro@protonmail.com>
2024-10-13 10:54:17 -0300

    checkpoint: comentarios e casos de teste simples

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/bash
 
-INPUTS=$(ls tests/e1 | grep entrada_)
+INPUTS=$(ls tests/e2 | grep entrada_)
 mkdir output/
 
 for file in $INPUTS; do
-  cat tests/e1/$file >  output/output_$file.txt
+  cat tests/e2/$file >  output/output_$file.txt
   echo '\n' >> output/output_$file.txt
-  cat tests/e1/$file | ./etapa1 >> output/output_$file.txt
+  cat tests/e2/$file | ./etapa2 >> output/output_$file.txt
   # Your code to process each file
 done

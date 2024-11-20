e039aca6a947b6b94340070477e16b8baf15e73b
Henrique Utzig <henrique.utzig@sap.com>
2024-11-20 09:26:24 -0300

    add symbol_table data struct

---
+++ src/symbol_table/symbol_table.c
@@ -0,0 +1,88 @@
+#include "symbol_table.h"
+
+int hash (char *lexema)
+{
+    int hash = 0;
+
+    for (int i = 0; lexema[i] != '\0'; i++)
+        hash = 31 * hash + lexema[i];
+
+    return hash % HASH_SIZE;
+}
+
+symbol_table_t *create_symbol_table()
+{
+    symbol_table_t *table = (symbol_table_t *)malloc(sizeof(symbol_table_t));
+
+    for (int i = 0; i < HASH_SIZE; i++)
+        table[i].next = NULL;
+
+    return table;
+}
+
+void destroy_symbol_table(symbol_table_t *table)
+{
+    for (int i = 0; i < HASH_SIZE; i++)
+    {
+        symbol_table_t *current = table[i].next;
+        while (current != NULL)
+        {
+            symbol_table_t *next = current->next;
+            free(current->lexema);
+            free(current);
+            current = next;
+        }
+    }
+
+    free(table);
+}
+
+symbol_table_t *insert(symbol_table_t *table, char *lexema, content_t *content)
+{
+    int index = hash(lexema);
+
+    symbol_table_t *new = (symbol_table_t *)malloc(sizeof(symbol_table_t));
+    new->lexema = strdup(lexema);
+    new->content = content;
+
+    new->next = table[index].next;
+    table[index].next = new;
+
+    return table;
+}
+
+symbol_table_t *remove(symbol_table_t *table, char *lexema)
+{
+    int index = hash(lexema);
+
+    symbol_table_t *current = &table[index];
+    while (current->next != NULL)
+    {
+        if (strcmp(current->next->lexema, lexema) == 0)
+        {
+            symbol_table_t *next = current->next->next;
+            free(current->next->lexema);
+            free(current->next);
+            current->next = next;
+            break;
+        }
+        current = current->next;
+    }
+
+    return table;
+}
+
+content_t *search(symbol_table_t *table, char *lexema)
+{
+    int index = hash(lexema);
+
+    symbol_table_t *current = table[index].next;
+    while (current != NULL)
+    {
+        if (strcmp(current->lexema, lexema) == 0)
+            return current->content;
+        current = current->next;
+    }
+
+    return NULL;
+}
---
+++ src/symbol_table/symbol_table.h
@@ -0,0 +1,25 @@
+#ifndef _SYMBOL_TABLE_H_
+#define _SYMBOL_TABLE_H_
+
+#include "content.h"
+
+#define HASH_SIZE 999
+
+typedef struct symbol_table
+{
+    char *lexema;
+    content_t *content;
+    struct symbol_table *next;
+} symbol_table_t;
+
+int hash (char *lexema);
+
+symbol_table_t *create_symbol_table();
+void destroy_symbol_table(symbol_table_t *table);   
+
+symbol_table_t *insert(symbol_table_t *table, char *lexema, content_t *content);
+symbol_table *remove(symbol_table_t *table, char *lexema);
+
+content_t *search(symbol_table_t *table, char *lexema);
+
+#endif

e120e3f825a1c0a7ffebef1b4138c1c7a7ea933a
Joao Cosme <joao.pedro.cosme@sap.com>
2024-12-15 11:14:55 -0300

    clean

--- src/code_generator/code_generator.c
+++ src/code_generator/code_generator.c
@@ -14,38 +14,6 @@ char *parse_unsigned_int(unsigned int value) {
     return str;
 }
 
-inst_block_t* generate_load_literal(char *valor, char *temp) {
-    inst_t *inst = create_inst(LOAD_I, valor, temp, NULL, NULL);
-    inst_block_t *block = create_inst_block(inst, NULL);
-    return block;
-}
-
-inst_block_t* generate_atribuicao(asd_tree_t *target, asd_tree_t *expr,unsigned int target_offset) {
-    inst_t *inst = create_inst(STORE_AI, expr->temp, "rfp",parse_unsigned_int(target_offset) ,NULL);
-    inst_block_t *block = create_inst_block(inst, NULL);    
-    block = append_inst_block(expr->code,block);
-
-    return block;
-}
-
-inst_block_t* generate_load(lexema *literal, char *temp) {
-    inst_t *inst = create_inst(LOAD_I, literal->valor, temp, NULL, NULL);
-    inst_block_t *block = create_inst_block(inst, NULL);
-    return block;
-}
-
-inst_block_t* generate_expression(char *target_temp, char *op1, char *op2,op_t operation){
-    inst_t *inst = create_inst(operation, op1, op2, target_temp, NULL);
-    inst_block_t *block = create_inst_block(inst, NULL);
-    return block;
-}
-
-inst_block_t* generate_load_ident(asd_tree_t *base, char *temp,unsigned int target_offset){
-    inst_t *inst = create_inst(LOAD_AI, "rfp",parse_unsigned_int(target_offset),temp ,NULL);
-    inst_block_t *block = create_inst_block(inst, NULL);
-    return block;
-}
-
 int is_integer(const char *str) {
     if (str == NULL || *str == '\0') {
         return 0;
@@ -55,6 +23,35 @@ int is_integer(const char *str) {
     bool is_int = *p == '\0';
     return is_int;
 }
+
+inst_block_t* generate_load_literal(char *valor, char *temp) {
+    inst_t *inst = create_inst(LOAD_I, valor, temp, NULL, NULL);
+    return create_inst_block(inst, NULL);
+}
+
+inst_block_t* generate_atribuicao(asd_tree_t *target, asd_tree_t *expr,unsigned int target_offset) {
+    inst_t *inst = create_inst(STORE_AI, expr->temp, "rfp",parse_unsigned_int(target_offset) ,NULL);
+    inst_block_t *block = create_inst_block(inst, NULL);    
+    block = append_inst_block(expr->code,block);
+    return block;
+}
+
+inst_block_t* generate_load(lexema *literal, char *temp) {
+    inst_t *inst = create_inst(LOAD_I, literal->valor, temp, NULL, NULL);
+    return create_inst_block(inst, NULL);
+}
+
+inst_block_t* generate_expression(char *target_temp, char *op1, char *op2,op_t operation){
+    inst_t *inst = create_inst(operation, op1, op2, target_temp, NULL);
+    return create_inst_block(inst, NULL);
+}
+
+inst_block_t* generate_load_ident(asd_tree_t *base, char *temp,unsigned int target_offset){
+    inst_t *inst = create_inst(LOAD_AI, "rfp",parse_unsigned_int(target_offset),temp ,NULL);
+    return create_inst_block(inst, NULL);
+}
+
+
 
 inst_block_t *generate_load_inner(asd_tree_t *node, char *temp, stackt_t *stack) {
     if (is_integer(node->label)) {

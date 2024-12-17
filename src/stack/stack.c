d2d03e59fd16fe02c693f9b2608e91b9bb367981
Joao Cosme <joao.pedro.cosme@sap.com>
2024-12-17 09:30:49 -0300

    remove refactor

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -5,37 +5,51 @@
     # 	João Pedro Cosme - 00314792		#
     #####################################
 */
+
 #include "stack.h"
 
-stackt_t *create_stack() {
+stackt_t *create_stack()
+{
     stackt_t *stack = (stackt_t *)malloc(sizeof(stackt_t));
 
     for (int i = 0; i < STACK_SIZE; i++)
-        stack->tables[i] = NULL;
+       stack->tables[i] = NULL;
 
     stack->top_index = -1;
     return stack;
 }
 
-bool is_empty(stackt_t *stack) {
+char *get_type_name(type_t type){
+    switch (type)
+    {
+    case INT:
+        return "int";
+    case FLOAT:
+        return "float";
+    default:
+        return "unknown";
+    }
+}
+
+bool is_empty(stackt_t *stack)
+{
     assert(stack != NULL);
     return stack->top_index == -1;
 }
 
-bool is_full(stackt_t *stack) {
+bool is_full(stackt_t *stack)
+{
     assert(stack != NULL);
     return stack->top_index == STACK_SIZE - 1;
 }
 
-void destroy_stack(stackt_t *stack) {
+void destroy_stack(stackt_t *stack)
+{
     assert(stack != NULL);
 
-    for (int i = 0; i <= stack->top_index; i++) {
-        if (stack->tables[i] != NULL) {
+    for (int i = 0; i < STACK_SIZE; i++)
+        if (stack->tables[i] != NULL)
             destroy_symbol_table(*stack->tables[i]);
-            free(stack->tables[i]);
-        }
-    }
 
     free(stack);
 }
@@ -43,7 +57,7 @@ void destroy_stack(stackt_t *stack) {
 stackt_t *push_symbol_table(stackt_t *stack, symbol_table_t *table) {
     assert(stack != NULL);
     assert(table != NULL);
-
+    // fprintf(stderr, "Pushing table %p to stack %p\n", table, stack);
     if (is_full(stack)) {
         printf("Stack is full\n");
         return stack;
@@ -51,119 +65,118 @@ stackt_t *push_symbol_table(stackt_t *stack, symbol_table_t *table) {
 
     symbol_table_t **table_ptr = (symbol_table_t **)malloc(sizeof(symbol_table_t *));
     assert(table_ptr != NULL);
-    *table_ptr = table;
+    *table_ptr = table; 
 
-    stack->tables[++stack->top_index] = table_ptr;
+    stack->top_index++;
+    stack->tables[stack->top_index] = table_ptr;
+
     return stack;
 }
 
 symbol_table_t *pop_symbol_table(stackt_t *stack) {
     assert(stack != NULL);
-
     if (is_empty(stack)) {
         return NULL;
     }
 
     symbol_table_t **table_ptr = stack->tables[stack->top_index];
+    assert(table_ptr != NULL);
     symbol_table_t *table = *table_ptr;
+    assert(table != NULL);
 
-    free(table_ptr);
-    stack->tables[stack->top_index--] = NULL;
+    free(table_ptr); 
+    stack->tables[stack->top_index] = NULL;
+    stack->top_index--;
     return table;
 }
 
-void insert_symbol(stackt_t *stack, lexema *lexema, int line, type_t type, unsigned int *offset, bool is_global) {
+void insert_symbol_to_global_scope(stackt_t *stack, lexema *lexema, int line, type_t type){
     assert(stack != NULL);
-    assert(offset != NULL || is_global);
-
     if (is_empty(stack)) {
-        printf("ERROR (insert_symbol): stack is empty! Could not insert symbol '%s'\n", lexema->valor);
+        printf("ERROR (insert_symbol_to_global_scope): stack is empty!\n");
         return;
     }
 
-    symbol_table_t *table = is_global ? *stack->tables[0] : *stack->tables[stack->top_index];
-    content_t *symbol = search_table(table, lexema->valor);
+    content_t *symbol = search_all_tables(stack, lexema->valor);
+    if (symbol != NULL) {
+        printf("Erro na linha %d: símbolo já declarado. O símbolo '%s' foi previamente declarado na linha %d.\n", line, lexema->valor, symbol->line);
+        exit(ERR_DECLARED);
+    }
 
+    content_t *content = create_content(line, lexema, type, FUNCTION, GLOBAL_BASE_POINTER, get_offset(*stack->tables[0], type));
+    insert_element(*stack->tables[0], lexema->valor, content);
+}
+
+void insert_symbol_to_scope(stackt_t *stack, lexema *lexema, int line, type_t type, unsigned int *offset){
+    assert(stack != NULL);
+    if (is_empty(stack)) {
+        printf("ERROR (insert_symbol_to_scope): stack is empty! Could not insert symbol '%s'\n", lexema->valor);
+        return;
+    }
+
+    symbol_table_t *table = *stack->tables[stack->top_index];
+    content_t *symbol = search_table(table, lexema->valor);
     if (symbol != NULL) {
         printf("Erro na linha %d: símbolo já declarado. O símbolo '%s' foi previamente declarado na linha %d.\n", line, lexema->valor, symbol->line);
         exit(ERR_DECLARED);
     }
 
     unsigned int var_type_size = (type == INT ? INT_SIZE : FLOAT_SIZE);
-    if (!is_global) {
-        *offset += var_type_size;
+    *offset += var_type_size;
+    content_t *content = create_content(line, lexema, type, VARIABLE, LOCAL_BASE_POINTER, *offset);
+    insert_element(*stack->tables[stack->top_index], lexema->valor, content);
+}
+
+void validate_attribution(stackt_t *stack, lexema *lexema, type_t type, int line){
+    if (is_empty(stack)) {
+        printf("ERROR (validate_attribution): stack is empty!\n");
+        return;
     }
 
-    content_t *content = create_content(line, lexema, type, is_global ? FUNCTION : VARIABLE, is_global ? GLOBAL_BASE_POINTER : LOCAL_BASE_POINTER, (is_global ? get_offset(*stack->tables[0], type) : *offset));
-    insert_element(table, lexema->valor, content);
+    content_t *symbol = search_all_tables(stack, lexema->valor);
+
+    if (symbol == NULL) {
+        printf("Erro na linha %d: símbolo não declarado. O símbolo '%s' não foi declarado.\n", line, lexema->valor);
+        exit(ERR_UNDECLARED);
+    }
+
+    if (symbol->nature != VARIABLE) {
+        printf("Erro na linha %d: função '%s' foi usada como variável!\n", symbol->line, lexema->valor);
+        exit(ERR_FUNCTION);
+    }
 }
 
-void insert_symbol_to_global_scope(stackt_t *stack, lexema *lexema, int line, type_t type) {
-    insert_symbol(stack, lexema, line, type, NULL, true);
-}
-
-void insert_symbol_to_scope(stackt_t *stack, lexema *lexema, int line, type_t type, unsigned int *offset) {
-    insert_symbol(stack, lexema, line, type, offset, false);
-}
-
-
-
-void validate_symbol_use(stackt_t *stack, lexema *lexema, int line, bool is_function) {
+void validate_function_call(stackt_t *stack, lexema *lexema, int line){
     assert(stack != NULL);
-
-    bool found = false;
+    bool found_as_variable = false;
+    bool found_as_function = false;
     for (int i = stack->top_index; i >= 0; i--) {
-        symbol_table_t *table = *(stack->tables[i]);
-        content_t *result = search_table(table, lexema->valor);
-
+        symbol_table_t *table = *(stack->tables[i]); 
+        // fprintf(stderr, "Searching lexema %s in table %d, with pointer %p\n",lexema->valor ,i, table);
+        content_t *result = search_table(table, lexema->valor); 
         if (result != NULL) {
-            found = true;
-            if ((is_function && result->nature != FUNCTION) || (!is_function && result->nature != VARIABLE)) {
-                printf("Erro na linha %d: %s '%s' foi usada como %s!\n", line, is_function ? "variável" : "função", lexema->valor, is_function ? "variável" : "função");
-                exit(is_function ? ERR_FUNCTION : ERR_VARIABLE);
+            // fprintf(stderr, "Found lexema %s in table %d\n",lexema->valor ,i);
+            if (result->nature == FUNCTION){
+                found_as_function = true;
+                break;
+            }else{
+                found_as_variable = true; 
             }
-            break;
         }
     }
-
-    if (!found) {
+    if(found_as_function){
+        // Tudo certo
+    }else if(found_as_variable){
+        printf("Erro na linha %d: variável '%s' foi usada como função!\n", line, lexema->valor);
+        exit(ERR_VARIABLE);
+    }else{
         printf("Erro na linha %d: símbolo não declarado. O símbolo '%s' não foi declarado.\n", line, lexema->valor);
         exit(ERR_UNDECLARED);
     }
 }
 
-void validate_attribution(stackt_t *stack, lexema *lexema, type_t type, int line) {
-    validate_symbol_use(stack, lexema, line, false);
-}
-
-void validate_function_call(stackt_t *stack, lexema *lexema, int line) {
-    validate_symbol_use(stack, lexema, line, true);
-}
-
-void validate_variable_use(stackt_t *stack, lexema *lexema, int line) {
-    validate_symbol_use(stack, lexema, line, false);
-}
-
-
-
-content_t *search_all_tables(stackt_t *stack, char *lexema) {
+unsigned int get_offset_from_stack(stackt_t *stack, char *valor){
     assert(stack != NULL);
-
-    for (int i = stack->top_index; i >= 0; i--) {
-        symbol_table_t *table = *(stack->tables[i]);
-        content_t *result = search_table(table, lexema);
-
-        if (result != NULL) {
-            return result;
-        }
-    }
-
-    return NULL;
-}
-
-unsigned int get_offset_from_stack(stackt_t *stack, char *valor) {
-    assert(stack != NULL);
-
     if (is_empty(stack)) {
         printf("ERROR (get_offset): stack is empty!\n");
         return 0;
@@ -174,14 +187,15 @@ unsigned int get_offset_from_stack(stackt_t *stack, char *valor) {
         printf("ERROR (get_offset): symbol %s not found!\n", valor);
         return 0;
     }
-
     return symbol->offset;
 }
 
+
+// -====================
+/// DEBUG FUNC
 void print_table(symbol_table_t *table, int stack_index) {
     printf("-----+-----+-----+-----+-----+\n");
     printf("StckId\tIndex\tLexema\tLine\tType\tNature\n");
-
     for (int i = 0; i < HASH_SIZE; i++) {
         symbol_table_t *current = &table[i];
         while (current != NULL) {
@@ -195,9 +209,49 @@ void print_table(symbol_table_t *table, int stack_index) {
 
 void print_all_tables(stackt_t *stack) {
     if (stack == NULL) return;
-
     for (int i = stack->top_index; i >= 0; i--) {
-        symbol_table_t *table = *(stack->tables[i]);
+        symbol_table_t *table = *(stack->tables[i]); 
         print_table(table, i);
     }
+}
+// -====================
+
+void validate_variable_use(stackt_t *stack, lexema *lexema, int line){
+    assert(stack != NULL);
+    bool found_as_variable = false;
+    bool found_as_function = false;
+    for (int i = stack->top_index; i >= 0; i--) {
+        symbol_table_t *table = *(stack->tables[i]); 
+        content_t *result = search_table(table, lexema->valor); 
+        if (result != NULL) {
+            if (result->nature == VARIABLE){
+                found_as_variable = true; 
+            } else if (result->nature == FUNCTION) {
+                found_as_function = true;
+                break; // Exit the loop if found as a function
+            }
+        }
+    }
+    if(found_as_function){
+        printf("Erro na linha %d: função '%s' foi usada como variável!\n", line, lexema->valor);
+        exit(ERR_FUNCTION);
+    }else if(found_as_variable){
+        // Everything is fine
+    }else{
+        printf("Erro na linha %d: símbolo não declarado. O símbolo '%s' não foi declarado.\n", line, lexema->valor);
+        exit(ERR_UNDECLARED);
+    }
+}
+
+
+content_t *search_all_tables(stackt_t *stack, char *lexema) {
+    assert(stack != NULL);
+    for (int i = stack->top_index; i >= 0; i--) {
+        symbol_table_t *table = *(stack->tables[i]); 
+        content_t *result = search_table(table, lexema); 
+        if (result != NULL) {
+            return result; 
+        }
+    }
+    return NULL; 
 }

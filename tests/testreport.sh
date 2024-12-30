d1a1a2f62eaf9e9fa767c3b2f826b8c374f50be5
Joao Cosme <joao.pedro.cosme@sap.com>
2024-12-30 10:18:36 -0300

    replace tests

--- tests/testreport.sh
+++ tests/testreport.sh
@@ -6,19 +6,20 @@ FAILED_TESTS=()
 
 rm -rf output
 rm -rf dot
-mkdir -p output/e5-prof
-mkdir -p dot/e5-prof
+mkdir -p output/e5
+mkdir -p dot/e5
 
 # Define the specific subfolder
-SUBFOLDER="tests/e5-prof"
+SUBFOLDER="tests/e5"
 
 if [ -d "$SUBFOLDER" ]; then
-  # INPUTS=$(ls "$SUBFOLDER" | grep entrada_)
-  INPUTS=$(ls "$SUBFOLDER" )
-  OUTPUT_DIR="output/e5-prof"
-  DOT_DIR="dot/e5-prof"
+  INPUTS=$(ls "$SUBFOLDER")
+  OUTPUT_DIR="output/e5"
+  DOT_DIR="dot/e5"
 
   for file in $INPUTS; do
+    STACK=$(($RANDOM + 100000))
+    DATA=$(($STACK / 2))
     OUTPUT_FILE="$OUTPUT_DIR/output_$file.txt"
     DOT_FILE="$DOT_DIR/output_$file.dot"
 
@@ -43,7 +44,7 @@ if [ -d "$SUBFOLDER" ]; then
       sh output2dot.sh < "$OUTPUT_FILE" > "$DOT_FILE"
       echo "DOT file generated: $DOT_FILE"
 
-      if timeout 10s python3 ilocsim.py -s -x < "$OUTPUT_FILE" > "$OUTPUT_DIR/ilocsim_$file.txt"; then
+      if timeout 3s python3 ilocsim.py --stack $STACK --data $DATA -s -x < "$OUTPUT_FILE" > "$OUTPUT_DIR/ilocsim_$file.txt"; then
         echo "Ilocsim result stored in: $OUTPUT_DIR/ilocsim_$file.txt"
         PASSED=$((PASSED + 1))
       else
